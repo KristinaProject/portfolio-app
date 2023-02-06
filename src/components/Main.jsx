f8024eb39d68ca387977854e36cfc7c8508ef970
KristinaProject <kristtule@gmail.com>
2023-02-06 09:23:46 +0200

    style: chaged main colors, working on animation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -27,19 +27,28 @@ function Trail({ open, children, ...props }) {
           className="trails-text"
           style={{
             ...rest,
-            transform: to(
-              [
-                x.to({
-                  range: [0, 0.9, 1],
-                  output: [0, 1.2, 1],
-                }),
-                y.to({
-                  range: [0, 0.9, 1],
-                  output: [0, 0.8, 1],
-                }),
-              ],
-              (x, y) => `scale(${x},${y})`
-            ),
+            transform: [
+              x.to({ range: [0, 0.9, 1], output: [0, 1.2, 1] }),
+              y.to({
+                range: [0, 0.9, 1],
+                output: [0, 0.8, 1],
+              }),
+            (x, y) => `scale(${x},${y})`
+            ],
+
+            // to(
+            // [
+            // x.to({
+            //   range: [0, 0.9, 1],
+            //   output: [0, 1.2, 1],
+            // }),
+            // y.to({
+            //   range: [0, 0.9, 1],
+            //   output: [0, 0.8, 1],
+            // }),
+            // ],
+            // (x, y) => `scale(${x},${y})`
+            // ),
           }}
         >
           {items[index]}
@@ -49,36 +58,37 @@ function Trail({ open, children, ...props }) {
   );
 }
 
-function Sqeez({ children }) {
-  const [state, toggle] = useState(false);
-  const { x, y } = useSpring({
-    from: { x: !state ? 1 : 0, y: !state ? 1 : 0 },
-    to: { x: 1, y: 1 },
-    config: { mass: 0.5, tension: 120, friction: 2, precision: 0.001 },
-    reset: state,
-    onStart: (state) => toggle(false),
-  });
+function Sqeez({ letter }) {
+  // const [state, toggle] = useState(false);
+  // const { x, y } = useSpring({
+  //   from: { x: !state ? 1 : 0, y: !state ? 1 : 0 },
+  //   to: { x: 1, y: 1 },
+  //   config: { mass: 0.5, tension: 120, friction: 2, precision: 0.001 },
+  //   reset: false,
+  //   reverse: state,
+  // onStart: (state) => toggle(false),
+  // });
 
   return (
     <animated.div
-      onMouseEnter={() => toggle(true)}
-      style={{
-        transform: to(
-          [
-            x.to({
-              range: [0, 0.3, 1],
-              output: [1, 1.3, 1],
-            }),
-            y.to({
-              range: [0, 0.3, 1],
-              output: [0, 0.7, 1],
-            }),
-          ],
-          (x, y) => `scale(${x},${y})`
-        ),
-      }}
+    // onMouseEnter={() => toggle(!state)}
+    // style={{
+    //   transform: to(
+    //     [
+    //       x.to({
+    //         range: [0, 0.3, 1],
+    //         output: [1, 1.3, 1],
+    //       }),
+    //       y.to({
+    //         range: [0, 0.3, 1],
+    //         output: [0, 0.7, 1],
+    //       }),
+    //     ],
+    //     (x, y) => `scale(${x},${y})`
+    //   ),
+    // }}
     >
-      {children}
+      {letter}
     </animated.div>
   );
 }
@@ -87,12 +97,14 @@ export function Main() {
   return (
     <div id="main">
       <Trail>
-        {Hello.map((word) => (
-          <Sqeez className="letter">{word}</Sqeez>
+        {Hello.map((letter) => (
+          <Sqeez className="letter" key={letter} letter={letter} />
         ))}
-        &nbsp;&nbsp;
-        {World.map((word) => (
-          <Sqeez className="letter">{word}</Sqeez>
+      </Trail>
+      &nbsp;&nbsp;
+      <Trail>
+        {World.map((letter) => (
+          <Sqeez className="letter" key={letter} letter={letter} />
         ))}
       </Trail>
     </div>
