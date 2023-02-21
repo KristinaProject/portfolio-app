9655ea928ee7303a57888fb6ef97b741bbad0d34
KristinaProject <kristtule@gmail.com>
2023-02-21 19:30:49 +0200

    feat: header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,28 @@ import { Link } from "react-router-dom";
 export function Header() {
   return (
     <header>
-      <div></div>
+      <div>KristinaProject</div>
       <ul>
-        <li><Link className="headerButton" to={'/'}>Home</Link></li>
-        <li><Link className="headerButton" to={'/about'}>About</Link></li>
-        <li><Link className="headerButton" to={'/work'}>Work</Link></li>
-        <li><Link className="headerButton" to={'/contact'}>Contact me!</Link></li>
+        <li>
+          <Link className="headerButton" to={"/"}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="headerButton" to={"/about"}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className="headerButton" to={"/work"}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link className="headerButton" to={"/contact"}>
+            Contact me!
+          </Link>
+        </li>
       </ul>
     </header>
   );
